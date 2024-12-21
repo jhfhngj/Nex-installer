de855749f780b82eb2c1b65305934940ced743a3
Ezzeldin Ahmed <99299697+jhfhngj@users.noreply.github.com>
2024-12-21 10:16:54 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 echo Getting Nex...
 git clone https://github.com/oXo23/Nex.git
-cd Nex
 pip install -r requirements.txt
 echo Doing extras to make sure...
-$dir = $((pwd))
-alias nexcompile="cd / |cd $dir | python3 main.py"
-echo Made alias for compiling as nexcompile
+mkdir $1
+copy Nex/ $1/
+copy $1
 echo Installed Nex.
